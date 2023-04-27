8caaecbb5ccbf102cb2f50b23a9421e3b17e8015
JacksonFA <jackson.f205@gmail.com>
2023-04-26 21:48:04 -0300

    Create structure

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,6 +1,24 @@
 module.exports = {
   extends: ["next", "turbo", "prettier"],
   rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "no-non-null-assertion": "off",
+    "indent": [
+        "error",
+        2
+    ],
+    "linebreak-style": [
+        "error",
+        "unix"
+    ],
+    "quotes": [
+        "error",
+        "single"
+    ],
+    "semi": [
+        "error",
+        "never"
+    ],
     "@next/next/no-html-link-for-pages": "off",
   },
   parserOptions: {

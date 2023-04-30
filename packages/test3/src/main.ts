2f13c9d476e069a0523a59b2b2096b0be99995d3
JacksonFA <jackson.f205@gmail.com>
2023-04-30 14:08:59 -0300

    update test3

--- packages/test3/src/main.ts
+++ packages/test3/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 5000;
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}`
+  );
 }
 bootstrap();
